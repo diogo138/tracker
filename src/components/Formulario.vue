556a6e7c8b128e3e02dab064467cb70844049090
diogo138 <diogomike138@gmail.com>
2024-10-15 05:48:51 -0300

    init

---
+++ src/components/Formulario.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column is-8" role="form" aria-label="formulario para criação de uma nova tarefa">
+                <input type="text" class="input" name="" id="" placeholder="Qual Tarefa você deseja iniciar?">
+            </div>
+            <div class="column">
+                <Temporizador />
+            </div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    import Temporizador from './Temporizador.vue'
+
+    export default defineComponent({
+        name: 'Formulario',
+        components: {
+            Temporizador
+        }
+    });
+</script>
+
+<style scoped>
+
+</style>
